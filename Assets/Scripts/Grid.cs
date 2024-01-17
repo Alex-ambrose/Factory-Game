577d4227f9729790467c86613a6a2cb19cb5fa16
ambid17 <ambid17@gmail.com>
2024-01-16 18:35:37 -0600

    add grid visualization

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -16,6 +16,27 @@ public class Grid
 
         gridArray = new int[width, height];
 
-        Debug.Log($"{width} {height}");
+        for(int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for(int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                VisualizeCell(x, y);
+            }
+        }
     }   
+
+    public void VisualizeCell(int x, int y)
+    {
+        GameObject gameObject = new GameObject($"Grid cell: {x},{y}");
+        gameObject.transform.localPosition = new Vector3(x, 0, y);
+        gameObject.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        gameObject.transform.localRotation = Quaternion.Euler(new Vector3(90, 0, 0));
+
+        TextMesh textMesh = gameObject.AddComponent<TextMesh>();
+        textMesh.text = $"{x},{y}";
+        textMesh.alignment = TextAlignment.Center;
+        textMesh.anchor = TextAnchor.MiddleCenter;
+        textMesh.fontSize = 50;
+
+    }
 }
